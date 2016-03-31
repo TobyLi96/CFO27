42b53a9d178719e99089d7c4febe943ab22b7c71
TobyUCL <zcabtli@ucl.ac.uk>
2016-03-31 22:42:05 +0100

    first commit

---
+++ src/comp207p/main/ConstantFolder.java
@@ -0,0 +1,142 @@
+package comp207p.main;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantPool;
+
+import org.apache.bcel.generic.*;
+import org.apache.bcel.util.InstructionFinder;
+
+
+
+public class ConstantFolder
+{
+	ClassParser parser = null;
+	ClassGen gen = null;
+
+	JavaClass original = null;
+	JavaClass optimized = null;
+
+	public ConstantFolder(String classFilePath)
+	{
+		try{
+			this.parser = new ClassParser(classFilePath);
+			this.original = this.parser.parse();
+			this.gen = new ClassGen(this.original);
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	private void optimizeMethod(ClassGen cgen, ConstantPoolGen cpgen, Method method) 
+	{
+		// Get the Code of the method, which is a collection of bytecode instructions
+		Code methodCode = method.getCode();
+
+		// Now get the actualy bytecode data in byte array, 
+		// and use it to initialise an InstructionList
+		InstructionList instList = new InstructionList(methodCode.getCode());
+
+		// Initialise a method generator with the original method as the baseline	
+		MethodGen methodGen = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), instList, cpgen);
+
+		// InstructionHandle is a wrapper for actual Instructions
+		for (InstructionHandle handle : instList.getInstructionHandles())
+		{
+			if (handle.getInstruction() instanceof StoreInstruction) {
+				if (handle.getPrev().getInstruction() instanceof BIPUSH) {
+					Number lastStackPush = ((BIPUSH) handle.getPrev().getInstruction()).getValue();
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof SIPUSH) {
+					Number lastStackPush = ((SIPUSH) handle.getPrev().getInstruction()).getValue();
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof ICONST) {
+					Number lastStackPush = ((ICONST) handle.getPrev().getInstruction()).getValue();
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof DCONST) {
+					Number lastStackPush = ((DCONST) handle.getPrev().getInstruction()).getValue();
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof FCONST) {
+					Number lastStackPush = ((FCONST) handle.getPrev().getInstruction()).getValue();
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof LCONST) {
+					Number lastStackPush = ((LCONST) handle.getPrev().getInstruction()).getValue();
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof LDC) {
+					Number lastStackPush = (Number) ((LDC) handle.getPrev().getInstruction()).getValue(cpgen);
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+				if (handle.getPrev().getInstruction() instanceof LDC2_W) {
+					Number lastStackPush = ((LDC2_W) handle.getPrev().getInstruction()).getValue(cpgen);
+					System.out.println(handle.getPrev().getInstruction());
+					System.out.println(lastStackPush);
+				}
+			}
+		}
+
+		// setPositions(true) checks whether jump handles 
+		// are all within the current method
+		instList.setPositions(true);
+
+		// set max stack/local
+		methodGen.setMaxStack();
+		methodGen.setMaxLocals();
+
+		// generate the new method with replaced iconst
+		Method newMethod = methodGen.getMethod();
+		// replace the method in the original class
+		cgen.replaceMethod(method, newMethod);
+	}
+
+	public void optimize()
+	{
+		ClassGen cgen = new ClassGen(original);
+		ConstantPoolGen cpgen = cgen.getConstantPool();
+		cgen.setMajor(50);
+		// Implement your optimization here
+		Method[] methods = cgen.getMethods();
+    for (Method m: methods) {
+        optimizeMethod(cgen, cpgen, m);
+    }
+        
+		this.optimized = cgen.getJavaClass();
+	}
+
+	
+	public void write(String optimisedFilePath)
+	{
+		this.optimize();
+
+		try {
+			FileOutputStream out = new FileOutputStream(new File(optimisedFilePath));
+			this.optimized.dump(out);
+		} catch (FileNotFoundException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/comp207p/target/ConstantVariableFolding.java
@@ -0,0 +1,32 @@
+package comp207p.target;
+
+public class ConstantVariableFolding
+{
+    public int methodOne(){
+        // int a = 429879283 - 876987;
+        // System.out.println(527309 -1293 + 5 * a);
+        // return 527309 -1293 + 5 * a;
+        int a = 62;
+        int b = (a + 764) * 3;
+        return b + 1234 - a;
+    }
+
+    public double methodTwo(){
+        double i = 0.67;
+        int j = 1;
+        return i + j;
+    }
+
+    public boolean methodThree(){
+        int x = 12345;
+        int y = 54321;
+        return x > y;
+    }
+
+    public boolean methodFour(){
+        long x = 4835783423L;
+        long y = 400000;
+        int z = 50;
+        return x > y;
+    }
+}
